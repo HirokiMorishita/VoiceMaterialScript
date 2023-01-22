e1cbb4a43e9e1a589e9b5668d052c4ccc28505ac
asumanino <asumanino@gmail.com>
2023-01-22 19:36:55 +0000

    fix

--- extract_vocal.sh
+++ extract_vocal.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -ux;
 input_file=$(fdfind --no-ignore -p -g '**/original/*.wav' | fzf)
 if [ -n "$input_file" ]; then
   demucs -n htdemucs_ft -o $(dirname $input_file)/../vocal_only/ --filename "{stem}-$(basename $input_file)" --two-stems=vocals $input_file 
