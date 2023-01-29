30ba8f4090d1ca3562ab731b6fe0f7f465b0dc21
asumanino <asumanino@gmail.com>
2023-01-29 10:03:11 +0000

    fix

--- generate_index.sh
+++ generate_index.sh
@@ -2,6 +2,10 @@
 
 splitted_voice_directory=$(fdfind --no-ignore -p -g '**/splitted/*'| fzf)
 if [ -n "$splitted_voice_directory" ] ; then
+  if [ -f "$splitted_voice_directory/index.csv" ]; then
+    echo "index.csv already exist"
+    exit
+  fi
   python3 src/generate_index.py $splitted_voice_directory $splitted_voice_directory/index.csv
   code --reuse-window $splitted_voice_directory/index.csv
 fi
