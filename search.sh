a3d8445d3ea58429c78a48ca952522eb81146ea4
asumanino <asumanino@gmail.com>
2023-02-04 03:33:54 +0000

    fix

--- search.sh
+++ search.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-read -r index_file_path target_line <<< "$(rg --no-ignore --no-heading -v '^\s*$' ./voice/*/splitted/*/index.csv 2>/dev/null | fzf --query "'" | awk -F: '{print $1, $2}')"
+read -r index_file_path target_line <<< "$(fdfind -I index.csv . | rg "${1:-.*}" | xargs rg --no-ignore --no-heading -v '^\s*$' 2>/dev/null | fzf --query "'" --delimiter ':' --nth 2.. | awk -F: '{print $1, $2}')"
 ( [[ -z "$index_file_path" ]] || [[ -z "$target_line" ]] ) && exit
 target_file_path="$(dirname $index_file_path)/$(echo $target_line | cut -d , -f 1)"
 
